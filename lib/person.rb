034578da2d8cc202c68bfd7f259b0c2056764782
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-05-15 12:50:20 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    
+    def name=(name)
+        @name = name
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+
+    def job
+        @job
+    end
+
+end
+
